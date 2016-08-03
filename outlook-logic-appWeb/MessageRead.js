e5fbae91c6037d2a2480f6345da6cc3b5fc1a8c0
jeffhollan <jeffhollan@outlook.com>
2016-08-03 11:43:42 -0700

    send data response page

--- outlook-logic-appWeb/MessageRead.js
+++ outlook-logic-appWeb/MessageRead.js
@@ -9,8 +9,13 @@
 
     var messageBanner;
 
+
     function reqListener() {
-        $('#message').text('Sent to Logic App');
+        var element = document.querySelector('.ms-MessageBanner');
+        messageBanner = new fabric.MessageBanner(element);
+        messageBanner.showBanner();
+        $('#main-grid').hide();
+        $('#result-link').show();
     }
 
     function send_request(item, result, url) {
@@ -19,15 +24,27 @@
         xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
         xhr.addEventListener('load', reqListener);
         item.bodyHTML = result.value;
+        item.form_priority = $('#priority').val();
+        item.form_title = $('#title').val();
+        item.form_assigned = $('#assigned').val();
         xhr.send(JSON.stringify(item));
     }
 
     // The Office initialize function must be run each time a new page is loaded.
     Office.initialize = function (reason) {
         $(document).ready(function () {
+            //document.getElementById("copyButton").addEventListener("click", function () {
+            //    copyToClipboard(document.getElementById('input-field-link'));
+            //    $('#link-div').focus();
+            //});
+
+            var item = Office.context.mailbox.item;
             var element = document.querySelector('.ms-MessageBanner');
             messageBanner = new fabric.MessageBanner(element);
             messageBanner.hideBanner();
+            $('#result-link').hide()
+
+            $('#title').val(item.subject);
 
             //loadProps();
             $('#bug').click(function ()
@@ -52,6 +69,55 @@
         });
     }
 
+    function copyToClipboard(elem) {
+        // create hidden text element, if it doesn't already exist
+        var targetId = "_hiddenCopyText_";
+        var isInput = elem.tagName === "INPUT" || elem.tagName === "TEXTAREA";
+        var origSelectionStart, origSelectionEnd;
+        if (isInput) {
+            // can just use the original source element for the selection and copy
+            target = elem;
+            origSelectionStart = elem.selectionStart;
+            origSelectionEnd = elem.selectionEnd;
+        } else {
+            // must use a temporary form element for the selection and copy
+            target = document.getElementById(targetId);
+            if (!target) {
+                var target = document.createElement("textarea");
+                target.style.position = "absolute";
+                target.style.left = "-9999px";
+                target.style.top = "0";
+                target.id = targetId;
+                document.body.appendChild(target);
+            }
+            target.textContent = elem.textContent;
+        }
+        // select the content
+        var currentFocus = document.activeElement;
+        target.focus();
+        target.setSelectionRange(0, target.value.length);
+
+        // copy the selection
+        var succeed;
+        try {
+            succeed = document.execCommand("copy");
+        } catch (e) {
+            succeed = false;
+        }
+        // restore original focus
+        if (currentFocus && typeof currentFocus.focus === "function") {
+            currentFocus.focus();
+        }
+
+        if (isInput) {
+            // restore prior selection
+            elem.setSelectionRange(origSelectionStart, origSelectionEnd);
+        } else {
+            // clear temporary content
+            target.textContent = "";
+        }
+        return succeed;
+    }
 
 
 })();
