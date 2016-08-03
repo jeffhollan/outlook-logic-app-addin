ff3fbfd65145d7eabd2a22d3848ad985d6badc21
jeffhollan <jeffhollan@outlook.com>
2016-08-02 20:50:14 -0700

    gulp, bower, npm config

--- outlook-logic-app-web-core/gulpfile.js
+++ outlook-logic-app-web-core/gulpfile.js
@@ -1,5 +1,34 @@
-﻿var gulp = require('gulp');
+﻿'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var lib = require('bower-files')();
+
+gulp.task('sass', ['clean'], function (callback) {
+
+         gulp.src('**/*.scss')
+          .pipe(sass().on('error', sass.logError))
+          .pipe(concat('lib.css'))
+          .pipe(gulp.dest('./wwwroot/dist/css'));
+         callback();
+    });
+   
+gulp.task('bower', ['sass'], function (callback) {
+    gulp.src(lib.ext('js').files)
+        .pipe(concat('lib.js'))
+        .pipe(gulp.dest('./wwwroot/dist/js'));
+    callback();
+});
+
+gulp.task('clean', require('del').bind(null, ['./wwwroot/dist']));
+
+gulp.task('uglify', ['sass', 'bower'], function () {
+
+});
+
 
 gulp.task('default', function () {
-    // Do work
+    return gulp.start('bower');
 });
